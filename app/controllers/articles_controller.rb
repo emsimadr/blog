7dd3d47dd00855e8b0d990866d4cbdb23adea3a1
Michael Simader <michael@simader.me>
2014-12-11 15:44:09 -0600

    added verification and error handling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,7 @@ class ArticlesController < ApplicationController
   end
   
   def new
+    @article = Article.new
   end
   
   def edit
@@ -16,8 +17,10 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
   end
   
   def update
